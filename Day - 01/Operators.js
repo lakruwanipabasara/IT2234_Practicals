c27a65076bb4f148cb618cb074bb3c46a12f8a64
lakruwanipabasara <lakruwanipabasara14@gmail.com>
2025-03-12 00:23:55 +0530

    Add files via upload

---
+++ Day - 01/Operators.js
@@ -0,0 +1,14 @@
+console.log("Arithmetic Operators:")
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let mul = num1 * num2;
+
+console.log("Summation is:" + sum);
+console.log("Subtraction is:" + sub);
+console.log("Division is:" + div);
+console.log("Multiplication is:" + mul);
